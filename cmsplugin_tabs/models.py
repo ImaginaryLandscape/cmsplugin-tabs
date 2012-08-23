baab8e8ea5e42c1260adb252fd55fe16ec509001
Tomas Neme <lacrymology@gmail.com>
2012-08-23 16:09:02 -0300

    preliminary models

---
+++ cmsplugin_tabs/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from cms.models import CMSPlugin
+from cmsplugin_tabs import utils
+
+TEMPLATE_CHOICES = utils.autodiscover_templates()
+
+class TabHeaderPlugin(CMSPlugin):
+    template = models.CharField()
+    tab_count = models.IntegerField()
+
+class TabTitle(models.Model):
+    title = models.CharField(max_length=64)
+    tab_header = models.ForeignKey(TabHeaderPlugin)
