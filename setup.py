f742c9d968f79c4471cf258432007b98f361e2d0
Tomas Neme <lacrymology@gmail.com>
2012-08-23 16:10:54 -0300

    typo

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     description = ('django-cms plugin to help rendering things like tabs or '
                    'accordion widgets'),
     packages=find_packages(),
-    provides=['cmsplugin-tabs', ],
+    provides=['cmsplugin_tabs', ],
     include_package_data=True,
     install_requires = [
         'django-inline-ordering>=0.1.1',
