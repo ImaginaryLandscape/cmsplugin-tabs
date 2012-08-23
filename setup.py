3ab1f39493057cfa8bac7691a48f08269b0cea8c
Tomas Neme <lacrymology@gmail.com>
2012-08-23 15:58:13 -0300

    setup.py and base package

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+from setuptools import find_packages, setup
+from cmsplugin_tabs import VERSION
+
+setup(
+    name='cmsplugin-tabs',
+    version=VERSION,
+    author='Tomas Neme',
+    author_email='lacrymology@gmail.com',
+    url='http://github.com/Lacrymology',
+    description = ('django-cms plugin to help rendering things like tabs or '
+                   'accordion widgets'),
+    packages=find_packages(),
+    provides=['cmsplugin-tabs', ],
+    include_package_data=True,
+    install_requires = [
+        'django-inline-ordering>=0.1.1',
+        'django-sekizai',
+        ],
+)
