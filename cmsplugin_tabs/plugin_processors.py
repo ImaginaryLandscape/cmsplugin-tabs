5fe0867a2b430d2169a27b98f5f183c06d45f368
Tomas Neme <lacrymology@gmail.com>
2012-08-25 15:07:18 -0300

    Change plugin processor so the wrapping code can be defined in the template

--- cmsplugin_tabs/plugin_processors.py
+++ cmsplugin_tabs/plugin_processors.py
@@ -1,20 +1,30 @@
 from cmsplugin_tabs.models import TabHeaderPlugin
+from django.template.loader import find_template
 
 def tabs_plugin_processor(instance, placeholder, rendered_content, original_context):
     request = original_context['request']
     if isinstance(instance, TabHeaderPlugin):
-        request.tab_count = instance.tab_count
-        request.tab_header = instance
-        return '{}\n<div class="tab-content">'.format(rendered_content)
+        wrap_info = {
+            'wrapper_plugin': instance,
+            'context': original_context,
+            'plugins': [],
+            'plugin_counter': instance.tab_count,
+            }
+        request.wrap_info = wrap_info
     else:
-        todo = getattr(request, 'tab_count', 0)
-        if todo:
-            tab_header = request.tab_header
-            request.tab_count -= 1
-            retval = '<div class="tab-pane" id="tab-{}-{}">\n{}\n</div>'.format(
-                tab_header.id, tab_header.tab_count - request.tab_count,
-                rendered_content)
-            if request.tab_count == 0 or original_context['plugin']['last']:
-                retval = "{}\n</div>".format(retval)
-            return retval
-    return rendered_content
+        wrap_info = getattr(request, 'wrap_info', None)
+        todo = wrap_info and wrap_info['plugin_counter']
+        if todo and not(instance._render_meta.text_enabled and instance.parent):
+            wrap_info['plugin_counter'] -= 1
+            wrap_info['plugins'].append(rendered_content)
+            if wrap_info['plugin_counter'] == 0 or original_context['plugin']['last']:
+                wrapper_plugin = wrap_info['wrapper_plugin']
+                template = find_template(wrapper_plugin.template)[0]
+                context = wrap_info['context']
+                context['plugins'] = wrap_info['plugins']
+                request.wrap_info = None
+                return template.render(context)
+        else:
+            # we're not in a wrapper, just return what we got untouched
+            return rendered_content
+    return u""
